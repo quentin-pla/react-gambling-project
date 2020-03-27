ea7cc21a5e5356c5e26d8b29ac123a2710916bf0
Quentin Pla <quentin-pla@hotmail.fr>
2020-03-27 23:07:41 +0100

    Essai résolution problème heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const Users = require("./src/database/models/User");
 const connect = require("./src/database/dbconnection");
