2941de39ff90b3c72c8dfc1d630254048f2a2d9b
Quentin Pla <quentin-pla@hotmail.fr>
2020-03-28 15:22:57 +0100

    test resolution pb heroku

--- server/server.js
+++ server/server.js
@@ -12,8 +12,6 @@ const connect = require("../react-ui/src/database/dbconnection");
 const messages = [{name: 'bot', text: 'Bienvenue.'}];
 
 io.on('connection', function (client) {
-    client.connect("https://nodejs-gambling.herokuapp.com/");
-
     //Connexion
     client.on('login', function (username, password) {
         //Connexion à la base de données
