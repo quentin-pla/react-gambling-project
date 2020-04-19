ac8c5955ce318523500d25925dbe2eecba7b92ad
Quentin Pla <quentin-pla@hotmail.fr>
2020-04-19 21:58:16 +0200

    Application

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,134 +1,113 @@
-import React, {useEffect, useState} from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
-} from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect,} from "react-router-dom";
 import {Navbar, Nav, Row, Col} from "react-bootstrap";
 import "./App.css";
 import PrivateRoute from "./components/routing/PrivateRoute";
-import Home from "./components/pages/Home";
-import Roulette from "./components/pages/Roulette";
-import Crash from "./components/pages/Crash";
-import Jackpot from "./components/pages/Jackpot";
-import Login from "./components/pages/Login";
-import Signup from './components/pages/Signup';
-import {useAuth} from './context/AuthContext';
+import Home from "./components/Home";
+import Roulette from "./components/games/roulette/Roulette";
+import Login from "./components/auth/Login";
+import Signup from './components/auth/Signup';
 import AuthButton from './components/auth/AuthButton';
 import Chat from "./components/chat/Chat";
 import $ from "jquery";
 import "jquery.scrollto";
+import {AuthContext} from "./context/AuthContext";
 
-export default function App() {
-    const [username, setUsername] = useState(false);
-    const [money, setMoney] = useState(false);
+class App extends Component {
+    /**
+     * Contexte d'authentification
+     */
+    static contextType = AuthContext;
 
-    const chat = <Chat name={username} scrollDown={handleChatScroll}/>;
+    constructor(props, context) {
+        super(props, context);
 
-    //Évènement de mise à jour des données de l'utilisateur
-    function handleUpdateUserData(username, money) {
-        setUsername(username);
-        setMoney(money);
+        this.handleChatScroll = this.handleChatScroll.bind(this);
     }
 
-    //Évènement de mise à jour de l'argent de l'utilisateur
-    function handleUpdateMoney(money) {
-        setMoney(money);
+    //Évènement appelé pour scroll en bas du chat lors de l'ajout d'un nouveau message
+    handleChatScroll() {
+        //Récupération de la liste des messages dans le DOM
+        let message_list = $('#message-list');
+        //Si la hauteur de la liste est supérieure à celle de l'écran (au moment où une scrollbar apparait)
+        if (message_list[0].scrollHeight > message_list[0].clientHeight)
+            //Scroll au dernier message
+            message_list.scrollTo('li:last-child');
     }
 
-    //Évènement appelé pour scroll en bas du chat
-    function handleChatScroll() {
-        $('#message-list').scrollTo('li:last-child');
-    }
+    render() {
+        //Chat affiché dans les pages seulement si connecté
+        const chat = (this.context.authenticated)
+            ? <Chat scrollDown={this.handleChatScroll}/>
+            : null;
 
-    function ChatContainer({children}) {
         return (
-            <Row className="m-0 full-height">
-                <Col className="col-md-1 col-md-push-9 message-list-fader"/>
-                <Col className="chat-container">
-                    {chat}
-                </Col>
-                <Col className="col-9 p-3 pl-5">
-                    {children}
-                </Col>
-            </Row>
+            <Router>
+                <Navbar className="navbar-top" variant="dark">
+                    <Navbar.Brand>
+                        <Link className="navbar-brand" to="/">
+                            <img alt="logo" src={process.env.PUBLIC_URL + '/logo.svg'} width="30" height="20"
+                                 className="d-inline-block mb-1 mr-2"/>
+                            Casino
+                        </Link>
+                    </Navbar.Brand>
+                    <NavLinks authenticated={this.context.authenticated}/>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <AuthButton/>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/login">
+                        <Login/>
+                    </Route>
+                    <Route path="/signup">
+                        <Signup/>
+                    </Route>
+                    <PrivateRoute path="/roulette">
+                        <ChatContainer chat={chat}>
+                            <Roulette/>
+                        </ChatContainer>
+                    </PrivateRoute>
+                    <Redirect to="/"/>
+                </Switch>
+            </Router>
         );
     }
+}
 
-    function NavLinks() {
-        const isAuthenticated = useAuth().authenticated;
-        return isAuthenticated ? (
-            <Nav className="mr-auto">
-                <Link className="nav-link" to="/roulette">Roulette</Link>
-                <Link className="nav-link" to="/crash">Crash</Link>
-                <Link className="nav-link" to="/jackpot">Jackpot</Link>
-            </Nav>
-        ) : (
-            <Nav className="mr-auto"/>
-        );
-    }
-
-    useEffect(() => {
-        $('#message-list').scrollTo(100);
-    }, []);
-
+/**
+ * Page contenant le chat à gauche
+ */
+function ChatContainer(props) {
     return (
-        <Router>
-            <Navbar className="navbar-top" variant="dark">
-                <Navbar.Brand>
-                    <Link className="navbar-brand" to="/">
-                        <img alt="logo" src={process.env.PUBLIC_URL + '/logo.svg'} width="30" height="20" className="d-inline-block mb-1 mr-2"/>
-                        Casino
-                    </Link>
-                </Navbar.Brand>
-                <NavLinks />
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        <AuthButton />
-                    </Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route path="/login">
-                    <Login onUpdateUserData={handleUpdateUserData}/>
-                </Route>
-                <Route path="/signup">
-                    <Signup onUpdateUserData={handleUpdateUserData}/>
-                </Route>
-                <PrivateRoute path="/roulette">
-                    <ChatContainer>
-                        <Roulette
-                            username={username}
-                            money={money}
-                            onUpdateMoney={handleUpdateMoney}
-                        />
-                    </ChatContainer>
-                </PrivateRoute>
-                <PrivateRoute path="/crash">
-                    <ChatContainer>
-                        <Crash
-                            username={username}
-                            money={money}
-                            onUpdateMoney={handleUpdateMoney}
-                        />
-                    </ChatContainer>
-                </PrivateRoute>
-                <PrivateRoute path="/jackpot">
-                    <ChatContainer>
-                        <Jackpot
-                            username={username}
-                            money={money}
-                            onUpdateMoney={handleUpdateMoney}
-                        />
-                    </ChatContainer>
-                </PrivateRoute>
-                <Redirect to="/"/>
-            </Switch>
-        </Router>
+        <Row className="m-0 full-height">
+            <Col className="message-list-fader d-none d-sm-block"/>
+            <Col className="chat-container d-none d-sm-block">
+                {props.chat}
+            </Col>
+            <Col className="game-container">
+                {props.children}
+            </Col>
+        </Row>
     );
 }
+
+/**
+ * Liens vers les différentes pages de l'application
+ */
+function NavLinks(props) {
+    return props.authenticated ? (
+        <Nav className="mr-auto">
+            <Link className="nav-link" to="/roulette">Roulette</Link>
+        </Nav>
+    ) : (
+        <Nav className="mr-auto"/>
+    );
+}
+
+export default App;
